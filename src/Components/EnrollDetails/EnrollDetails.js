c33ea491c8266905a9d56f4316405cd040685e80
ShuvoDas1 <67542645+ShuvoDas1@users.noreply.github.com>
2020-08-30 18:09:44 +0600

    online courses assignment coding complete

--- src/Components/EnrollDetails/EnrollDetails.js
+++ src/Components/EnrollDetails/EnrollDetails.js
@@ -5,14 +5,21 @@ const EnrollDetails = (props) => {
     console.log(enroll);
     const totalPrice = enroll.reduce((total,course)=>total+course.price,0)
     
+    let discount = 0;
+    if(enroll.length > 3 ){
+        discount = totalPrice / 10;
+    }
         
+    const grandTotal = totalPrice - discount;
       
     return (
         <div className='container'>
-            <h4>Enroll Summery</h4>
+            <h4 className="text-center">Enroll Summery</h4>
+            <br/>
             <h6>Enrolled Courses: {enroll.length}</h6>
-            {/* <p>Discount: {discount}</p> */}
-            <p>Total Price: ${totalPrice}</p>
+            <p>Course price: ${totalPrice.toFixed(2)}</p>
+            <p>Discount: ${discount.toFixed(2)}</p>
+            <p>Total Price: ${grandTotal.toFixed(2)}</p>
         </div>
     );
 };
